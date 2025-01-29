0df2e1d74a2e88b747a644d479247eec56b4fe87
Parsa <parsafathi.pf.2002@gmail.com>
2025-01-29 14:18:14 -0800

    اضافه کردن مدل هتل ، و اپدیت کردن دیتابیس

---
+++ Otaghche.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Otaghche.Domain.Entities;
+
+namespace Otaghche.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+
+
+
+    }
+}
--- Otaghche.Web/Program.cs
+++ Otaghche.Web/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Otaghche.Infrastructure.Data;
+
 namespace Otaghche.Web
 {
     public class Program
@@ -8,6 +11,9 @@ namespace Otaghche.Web
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<ApplicationDbContext>(option =>
+            option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
