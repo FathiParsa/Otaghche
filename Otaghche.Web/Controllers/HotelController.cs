076633bcf5d888d898728657577fb610f2298c27
Parsa <parsafathi.pf.2002@gmail.com>
2025-01-29 23:48:46 -0800

    عملیات حذف و آپدیت برای هتل ها پیاده سازی شد

--- Otaghche.Web/Controllers/HotelController.cs
+++ Otaghche.Web/Controllers/HotelController.cs
@@ -28,11 +28,58 @@ namespace Otaghche.Web.Controllers
         {
             if (ModelState.IsValid)
             {
-                await _context.AddAsync(hotel);
+                await _context.Hotels.AddAsync(hotel);
                 await _context.SaveChangesAsync();
                 return RedirectToAction("Index");
             }
             return View(hotel);
+        }
+
+        public async Task<IActionResult> Update(int hotelId)
+        {
+            Hotel? hotel = await _context.Hotels.FirstOrDefaultAsync(h => h.Id == hotelId);
+            if (hotel == null) 
+            { 
+                return NotFound();
+            }
+            return View(hotel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(Hotel hotel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Hotels.Update(hotel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(hotel);
+        }
+
+        public async Task<IActionResult> Delete(int hotelId)
+        {
+            var hotel = await _context.Hotels.FirstOrDefaultAsync( h => h.Id == hotelId);
+            if (hotel == null)
+            {
+                return NotFound();
+            }
+            return View(hotel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(Hotel hotel)
+        {
+            Hotel? correctHotel = await _context.Hotels.FirstOrDefaultAsync( h => h.Id == hotel.Id);
+
+            if (correctHotel == null)
+            { 
+                return NotFound();
+            }
+            
+            _context.Hotels.Remove(correctHotel);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
         }
     }
 }
