80bbaec2eae9d7f97d70ecd175091728dc792389
Parsa <parsafathi.pf.2002@gmail.com>
2025-01-29 18:13:54 -0800

    کنترلر هتل اضافه شد

---
+++ Otaghche.Web/Controllers/HotelController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Otaghche.Infrastructure.Data;
+
+namespace Otaghche.Web.Controllers
+{
+    public class HotelController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public HotelController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var hotels = await _context.Hotels.ToListAsync();
+            return View(hotels);
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+    }
+}
